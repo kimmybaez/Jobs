7b99763078d830497a27700d371b352831d5c70e
kimmybaez <90371389+kimmybaez@users.noreply.github.com>
2022-01-29 12:35:11 +0300

    Created a configuration file
    * Added feature "Console-broadcasts"
    * Added config file
    * Added "sendConsoleBroadcast" method

--- src/main/java/ru/jobs/jobs/Jobs.java
+++ src/main/java/ru/jobs/jobs/Jobs.java
@@ -1,17 +1,37 @@
 package ru.jobs.jobs;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.util.logging.Logger;
 
 public final class Jobs extends JavaPlugin {
 
+    private final Logger logger = Logger.getLogger("Jobs");
+    private final File config = new File(getDataFolder() + File.separator + "config.yml");
+    private boolean broadcastFactor = getConfig().getBoolean("console.console-broadcast");
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
 
+        if (!config.exists()) {
+            getConfig().options().copyDefaults(true);
+            saveDefaultConfig();
+        }
+
+        if (broadcastFactor) {
+            sendConsoleBroadcast(logger);
+        }
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    private void sendConsoleBroadcast(Logger sender) {
+        //Сделай еще броадкаст
+        sender.info("Plugin enabled");
     }
 }
