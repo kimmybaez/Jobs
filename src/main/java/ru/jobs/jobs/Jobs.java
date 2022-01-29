56cc0858c64d9c7f5f30afa14dfa83f2c3187593
kimmybaez <90371389+kimmybaez@users.noreply.github.com>
2022-01-29 14:13:37 +0300

    Work on creating database tables given Mysql

--- src/main/java/ru/jobs/jobs/Jobs.java
+++ src/main/java/ru/jobs/jobs/Jobs.java
@@ -1,37 +1,66 @@
 package ru.jobs.jobs;
 
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
+import java.sql.*;
 import java.util.logging.Logger;
 
 public final class Jobs extends JavaPlugin {
 
     private final Logger logger = Logger.getLogger("Jobs");
     private final File config = new File(getDataFolder() + File.separator + "config.yml");
-    private boolean broadcastFactor = getConfig().getBoolean("console.console-broadcast");
+    private boolean broadcastSwitcher = getConfig().getBoolean("console.console-broadcast");
+    private static Connection con;
+    private static Statement stmt;
+    private static ResultSet rs;
+    private final String address = getConfig().getString("database.ip");
+    private final String port = getConfig().getString("database.port");
+    private final String username = getConfig().getString("database.username");
+    private final String password = getConfig().getString("database.password");
+    private final String database = getConfig().getString("database.database");
+    private String jdbcURL = "jdbc:mysql://" + address + ":" + port + "/" + database;
 
     @Override
     public void onEnable() {
-
+        //Test for configuration
         if (!config.exists()) {
             getConfig().options().copyDefaults(true);
             saveDefaultConfig();
         }
-
-        if (broadcastFactor) {
-            sendConsoleBroadcast(logger);
+        //Logic for broadcastSwitcher
+        if (broadcastSwitcher) {
+            sendConsoleBroadcastOnEnable(logger);
         }
+
+
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        sendConsoleBroadcastOnDisable(logger);
     }
 
-    private void sendConsoleBroadcast(Logger sender) {
+    private void sendConsoleBroadcastOnEnable(Logger sender) {
         //Сделай еще броадкаст
         sender.info("Plugin enabled");
+    }
+
+    private void sendConsoleBroadcastOnDisable(Logger sender) {
+        //Сделай еще броадкаст
+        sender.info("Plugin disabled");
+    }
+
+    private void setConnection(Connection connection, String url, String login, String pass) {
+            connection = DriverManager.getConnection(url, login, pass);
+    }
+
+    private void closeConnection(Connection connection, Statement statement, ResultSet resultSet) {
+        try {
+            connection.close();
+            statement.close();
+            resultSet.close();
+        } catch (SQLException se) {
+        }
     }
 }
