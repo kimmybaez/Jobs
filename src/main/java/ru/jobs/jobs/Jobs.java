a5861c99d3bb7f2f75dce2c8c5ff413a988ef035
kimmybaez <90371389+kimmybaez@users.noreply.github.com>
2022-01-29 15:56:10 +0300

    Initial work with Mysql completed
    * Added "createTableMethod" for creating table when the plugin turned on
    * Added "closeConnection" for close mysql resourses when the plugin turned off
    *Added Sql Query in string variable for create table
    * Added connection logic when plugin turned on
    * Added connection logic when plugin turned off
    * 2 TODO tags added

--- src/main/java/ru/jobs/jobs/Jobs.java
+++ src/main/java/ru/jobs/jobs/Jobs.java
@@ -10,7 +10,8 @@ public final class Jobs extends JavaPlugin {
 
     private final Logger logger = Logger.getLogger("Jobs");
     private final File config = new File(getDataFolder() + File.separator + "config.yml");
-    private boolean broadcastSwitcher = getConfig().getBoolean("console.console-broadcast");
+    private final boolean broadcastSwitcher = getConfig().getBoolean("console.console-broadcast");
+    private final String dataFormat = getConfig().getString("database.format");
     private static Connection con;
     private static Statement stmt;
     private static ResultSet rs;
@@ -19,7 +20,12 @@ public final class Jobs extends JavaPlugin {
     private final String username = getConfig().getString("database.username");
     private final String password = getConfig().getString("database.password");
     private final String database = getConfig().getString("database.database");
-    private String jdbcURL = "jdbc:mysql://" + address + ":" + port + "/" + database;
+    private final String jdbcURL = "jdbc:mysql://" + address + ":" + port + "/" + database;
+    private final String table = "CREATE TABLE IF NOT EXISTS `" + database + "`.`jobs` (" +
+            "`id` INT NOT NULL AUTO_INCREMENT," +
+            "`player` VARCHAR(30) NOT NULL," +
+            "`job` VARCHAR(45) NULL," +
+            "PRIMARY KEY (`id`, `player`));";
 
     @Override
     public void onEnable() {
@@ -33,34 +39,62 @@ public final class Jobs extends JavaPlugin {
             sendConsoleBroadcastOnEnable(logger);
         }
 
+        if (dataFormat.equalsIgnoreCase("mysql")) {
+            setConnection(jdbcURL, username, password);
+            createTableMethod(table);
+        }
+
 
     }
 
     @Override
     public void onDisable() {
         sendConsoleBroadcastOnDisable(logger);
+        if (dataFormat.equalsIgnoreCase("mysql")) {
+            closeConnection(con, stmt, rs);
+        }
     }
 
+
+
+    //TODO: Сделать более красивое сообщение о выключении
     private void sendConsoleBroadcastOnEnable(Logger sender) {
-        //Сделай еще броадкаст
         sender.info("Plugin enabled");
     }
 
+    //TODO: Сделать более красивое сообщение о выключении
     private void sendConsoleBroadcastOnDisable(Logger sender) {
-        //Сделай еще броадкаст
         sender.info("Plugin disabled");
     }
 
-    private void setConnection(Connection connection, String url, String login, String pass) {
-            connection = DriverManager.getConnection(url, login, pass);
+    //is used to create a connection
+    private void setConnection(String url, String login, String pass) {
+        try {
+            con = DriverManager.getConnection(url, login, pass);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
+    //is used as a shutdown method for mysql data in the onDisable method
     private void closeConnection(Connection connection, Statement statement, ResultSet resultSet) {
         try {
             connection.close();
             statement.close();
             resultSet.close();
         } catch (SQLException se) {
+            se.printStackTrace();
         }
     }
+
+    //is used to create mysql database table
+    private void createTableMethod(String string) {
+        try {
+            stmt = con.createStatement();
+            stmt.executeUpdate(string);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
